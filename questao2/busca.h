77f2b1251e68a904d89b6aa9674ef5eaca9e85a5
Alex A. Rocha <lex99r@gmail.com>
2019-04-09 02:13:08 -0400

    incluindo questao 2 e alterando algumas coisas na questao 3

---
+++ questao2/busca.h
@@ -0,0 +1,4 @@
+// Busca binária, classe: O(log N)
+int buscaBinaria(int* vet, int N, int chave);
+// Busca sequencial, classe: O(N)
+int buscaSequencial(int* vet, int N, int chave);
--- questao3/main.c
+++ questao3/main.c
@@ -19,40 +19,40 @@ int main(void){
 
 	int vetA[N], vetB[N], vetC[N];
 	clock_t inicio, fim;
-	double tempo, tempoBolha = 0.0, tempoInsertion = 0.0, tempoQuick = 0.0;
+	double tempo, tempoQuick = 0.0, tempoInsertion = 0.0, tempoBolha = 0.0;
 
 	for(int cont = 0; cont < RODADAS; cont++){
 
 		preencherVetores(vetA, vetB, vetC);
 
-		printf("====== Rodada %d ======\n\n", cont + 1);
+		printf("======== Rodada %02d ========\n\n", cont + 1);
 
 		inicio = clock();
 		quickSort(vetA, 0, N);
 		fim = clock();
 		tempoQuick += tempo = (fim - inicio)/1000.0;
-		printf("- Quicksort: %.3lf ms\n", tempo);
+		printf("Quicksort: %.3lf ms\n", tempo);
 
 		inicio = clock();
 		insertionSort(vetB, N);
 		fim = clock();
 		tempoInsertion += tempo = (fim - inicio)/1000.0;
-		printf("- Inserção direta: %.3lf ms\n", tempo);
+		printf("Inserção direta: %.3lf ms\n", tempo);
 
 		inicio = clock();
 		bubbleSort(vetC, N);
 		fim = clock();
 		tempoBolha += tempo = (fim - inicio)/1000.0;
-		printf("- Bolha: %.3lf ms\n\n", tempo);
+		printf("Bolha: %.3lf ms\n\n", tempo);
 
 	}
 
-	printf("######################\n\n");
+	printf("###########################\n\n");
 
 	printf("Tempo médio de execução dos algoritmos de ordenação:\n\n");
-	printf("- Quicksort: %.4lf ms\n", tempoQuick/RODADAS);
-	printf("- Inserção Direta: %.4lf ms\n", tempoInsertion/RODADAS);
-	printf("- Bolha: %.4lf ms\n", tempoBolha/RODADAS);
+	printf("Quicksort: %.4lf ms\n", tempoQuick/RODADAS);
+	printf("Inserção Direta: %.4lf ms\n", tempoInsertion/RODADAS);
+	printf("Bolha: %.4lf ms\n", tempoBolha/RODADAS);
 
 	return 0;
 
